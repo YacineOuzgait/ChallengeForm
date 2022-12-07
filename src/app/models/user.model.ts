d1fa3bbd41e865c326a2f0701ad432ae3a32d1d1
Yacine Ouzgait <yacineouzgait59146@gmail.com>
2022-12-07 14:58:07 +0100

    Quests_DrivenForms

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,9 @@
+export class User{
+
+    constructor(
+        public lastName: string,
+        public firstName: string,
+        public email: string,
+        public password: string
+    ){}
+}
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
+  model: User = new User("","","","");
+  hidden: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit() : void{
+    this.hidden = true
+    console.log(this.model)
+  }
 }
